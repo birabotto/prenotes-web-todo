a3c1082999f3ae70db62837873744bbf5e3b7a48
Tibira Botto <tibira.botto@gmail.com>
2025-02-07 04:08:29 -0500

    space

--- src/pages/gf/GoodsFlow.tsx
+++ src/pages/gf/GoodsFlow.tsx
@@ -160,7 +160,7 @@ export default function GoodsFlow() {
       </div>
 
       <button
-        className="px-6 py-3 bg-[#0058a3] w-full text-[#d6bf00] font-semibold rounded-lg shadow-md hover:bg-[#004080] hover:text-[#ffd700] transition-all duration-300 mb-4 focus:outline-none focus:ring-2 focus:ring-[#d6bf00] focus:ring-opacity-50"
+        className="px-6 py-3 bg-[#0058a3] w-full text-[#d6bf00] font-semibold rounded-lg shadow-md hover:bg-[#004080] hover:text-[#ffd700] transition-all duration-300 mb-2 focus:outline-none focus:ring-2 focus:ring-[#d6bf00] focus:ring-opacity-50"
         onClick={() => handleSearch(item || "")}
       >
         Find
